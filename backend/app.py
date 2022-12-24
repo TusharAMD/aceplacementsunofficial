4b8290385a61b5db4ee61336efd84a4d6988ca65
Tushar <tusharvamanamdoskar@gmail.com>
2022-12-24 19:37:53 +0530

    4

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,12 @@ cors = CORS(app)
 @app.route("/listofcompanies")
 @cross_origin()
 def listofcompanies():
-    client = pymongo.MongoClient("mongodb+srv://atharvaplacementunofficial:x1zuiYEJN13prH9p@cluster0.vyjrl6b.mongodb.net/?retryWrites=true&w=majority")
-    db = client.test
+    client = pymongo.MongoClient("mongodb+srv://atharvaplacementunofficial:admin@cluster0.vyjrl6b.mongodb.net/?retryWrites=true&w=majority")
+    db = client.unofficialplacements
+    coll = db.companies
+    print(coll.find())
+    for x in coll.find():
+        print(x)
     return {}
 
 if __name__ == "__main__":
