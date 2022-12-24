af9eda6454c017ecddf1ce7932dc96a31bafb07d
Tushar <tusharvamanamdoskar@gmail.com>
2022-12-24 10:54:45 +0530

    2

--- frontend/acefront/src/Components/List.js
+++ frontend/acefront/src/Components/List.js
@@ -1,7 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
+import React, { useState } from 'react';
 
 function List() {
+    const [open, setOpen] = useState(false);
+
     return (
         <div className="list">
 
@@ -22,10 +26,21 @@ function List() {
 
         let currentStatusIndicator = {"Registration Open":"green","Ongoing":"#808080","Completed":"red"}
 
-        let array = [{ "Name": "Lenden", "Package": "7 LPA", "DatePosted": "21th Nov 2022", "Deadline": "22nd Nov 2022", "logo": "https://www.lendenclub.com/wp-content/uploads/2020/09/LDC_Light.png", "Updates": {
-            "1": {"msg": "Registration Phase", "link": "https://classroom.google.com/c/NDk2MjI5MTMyMTIy/p/NTA3NDQyMDUzODk3/details"},
-            "2":{"msg":"Shortlisted Students","link":"https://classroom.google.com/c/NDk2MjI5MTMyMTIy/p/NTA3NTE3MjE1ODQx/details"},
-            "3":{} } 
+        let array = [{ 
+            "Name": "Lenden Club", 
+            "Package": "7", 
+            "DatePosted": "21th Nov 2022", 
+            "Deadline": "22nd Nov 2022", 
+            "Logo": "https://www.lendenclub.com/wp-content/uploads/2020/09/LDC_Light.png",
+            "Status":"Completed",
+            "Discord":"", 
+            "Updates": {
+                "1": {"msg": "Registration Phase", "link": "https://classroom.google.com/c/NDk2MjI5MTMyMTIy/p/NTA3NDQyMDUzODk3/details"},
+                "2":{"msg":"Shortlisted Students are displayed on Classroom","link":"https://classroom.google.com/c/NDk2MjI5MTMyMTIy/p/NTA3NTE3MjE1ODQx/details"},
+                "3":{"msg":"Shortlisted Students are displayed on Classroom (2)","link":"https://classroom.google.com/c/NDk2MjI5MTMyMTIy/p/NTQwODI4ODkyODYw/details"},
+                "4":{"msg":"Interview Call","link":"https://classroom.google.com/c/NDk2MjI5MTMyMTIy/p/NTA5MDU3ODg3MTE0/details"},
+                "5":{"msg":"Final Selection Done","link":""},
+                } 
         }]
 
         return (<>
@@ -34,29 +49,46 @@ function List() {
 
                 return (
                     <>
-                        <Card>
-                            <Card.Img style={{ width: "10em" }} variant="top" src={item.logo} />
+                        <Card style={{backgroundColor:"rgba(255,255,255,0.7)"}}>
+                            <Card.Img style={{ width: "10em" }} variant="top" src={item.Logo} />
                             <Card.Body style={{ color: "black" }}>
 
                                 <Card.Title>{item.Name}</Card.Title>
-                                <Card.Text>
+                                <Card.Text style={{fontFamily: "'Poppins', sans-serif"}}>
+                                    
+                                    
                                     {/*item.Updates[item.Updates.length - 1]*/}
                                     {/*console.log(`${item.Updates[0]}`)*/}
                                     {/*console.log(`${Math.max(...Object.keys(item.Updates))}`)*/}
+                                    
+                                    <hr/>
+                                    <div><u>Latest Update:</u></div>
+                                    <div style={{border:"2px dashed black"}}>
+                                    {item.Updates[Math.max(...Object.keys(item.Updates))]["msg"]}<br/>
+                                    <a href={item.Updates[Math.max(...Object.keys(item.Updates))]["link"]}>Classroom Link</a>
+                                    </div>
                                 </Card.Text>
                                 <div className='carddiv'>
                                     <div>Date Posted: {item.DatePosted}</div>
                                     <div>Deadline: {item.Deadline}</div>
-                                    <div>Package: {item.Package}</div>
+                                    <div>Package: <span style={{color:"white",border:"2px dotted #012522",backgroundColor:"#016b62",borderRadius:"100%",padding:"5px"}}>{item.Package} LPA</span></div>
                                 </div>
                                 <br />
                                 <div className='carddiv'>
-                                    <div>Status: <span style={{ color: color1 }}>Completed</span></div>
-                                    <div><img style={{ width: "2em" }} src='https://cdn-icons-png.flaticon.com/512/2111/2111370.png'></img></div>
+                                    <div>Status: <span style={{ color: currentStatusIndicator[item["Status"]] }}>{item["Status"]}</span></div>
+                                    <div><a href="https://discord.gg/dffp5Cvyqj"><img style={{ width: "2em" }} src='https://cdn-icons-png.flaticon.com/512/2111/2111370.png'></img></a></div>
                                     <div>Add to WatchList</div>
                                 </div>
                                 <br />
-                                <Button variant="primary">Go somewhere</Button>
+                                
+                                <Button  onClick={() => setOpen(!open)} variant="primary">See all Updates</Button>
+                                
+                                <Collapse in={open}>
+                                    <div className='updatenotify'>
+                                        <Updatesnotifier item={item} />
+                                    </div>
+                                </Collapse>
+
                             </Card.Body>
                         </Card>
                         <br />
@@ -64,6 +96,33 @@ function List() {
                 )
 
             })}
+        </>)
+    }
+
+    function Updatesnotifier(props){
+        let updatearray = props.item.Updates;
+        console.log(updatearray)
+        return(<>
+        <br/>
+        <ol>
+        {/*JSON.stringify(props.item.Updates)*/}
+        {
+            Object.keys(updatearray).map((item,index)=>{
+                return(
+                <>
+                {/*JSON.stringify(item)*/}
+                
+                <li className='eachupdate'>
+                    <div>{item}</div>
+                    <div>{updatearray[item]["msg"]}</div>
+                    <div><a href={updatearray[item]["link"]}>Classroom Link</a></div>
+                </li>
+     
+                </>
+                )
+            })
+        }
+        </ol>
         </>)
     }
 }
